2e14817bd8d6c89d155e5c8b972b09d840e7abde
MOSKANOR <mostapha.boudad.02@gmail.com>
2024-04-24 21:57:06 +0800

    5

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -6,24 +6,28 @@
 from requests import get
 from sys import argv
 
-
 if __name__ == '__main__':
+    if len(argv) != 2:
+        print("Usage: python script.py <user_id>")
+        exit(1)
+
     user_id = argv[1]
-    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    response = get(url)
-    name = response.json().get('name')
+    try:
+        url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+        response = get(url)
+        response.raise_for_status()  # Raise an exception for bad responses
+        name = response.json().get('name')
 
-    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
-    response = get(url)
-    tasks = response.json()
-    done = 0
-    done_tasks = []
-    for task in tasks:
-        if task.get('completed'):
-            done_tasks.append(task)
-            done += 1
+        url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
+        response = get(url)
+        response.raise_for_status()
+        tasks = response.json()
+        done_tasks = [task for task in tasks if task.get('completed')]
+        done = len(done_tasks)
 
-    print("Employee {} is done with tasks({}/{}):"
-          .format(name, done, len(tasks)))
-    for task in done_tasks:
-        print("\t {}".format(task.get('title')))
+        print("Employee {} is done with tasks({}/{}):"
+              .format(name, done, len(tasks)))
+        for task in done_tasks:
+            print("\t{}".format(task.get('title')))
+    except Exception as e:
+        print("An error occurred:", e)
